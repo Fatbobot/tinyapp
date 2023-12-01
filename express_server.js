457b237a495ab5e182f3e3451b32887ab0a7892c
Paul Jean <Fatbobot@gmail.com>
2023-12-01 03:04:52 -0400

    update form to redirect client to newpage with shortened url and long url

--- express_server.js
+++ express_server.js
@@ -4,12 +4,15 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 app.post("/urls", (req, res) => {
+  const shortUrl = generateString();
+  urlDatabase[shortUrl] = req.body.longURL;
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  console.log(urlDatabase);
+  res.redirect(`/urls/${shortUrl}`);
 });
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -40,4 +43,15 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-function generateRandomString() {}
+const characters =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+const generateString = function () {
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+};
