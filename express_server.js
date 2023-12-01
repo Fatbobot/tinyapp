3a61ecb03b13ceebe8b29e69108f621482fcab7a
Paul Jean <Fatbobot@gmail.com>
2023-12-01 19:34:48 -0400

    add login feature and install cookie parser

--- express_server.js
+++ express_server.js
@@ -1,21 +1,27 @@
 const express = require("express");
+var cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
 app.post("/urls/:id/edit", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
-  res.redirect("/urls")
+  res.redirect("/urls");
 });
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
-  console.log(urlDatabase)
-  res.redirect("/urls")
-})
+  console.log(urlDatabase);
+  res.redirect("/urls");
+});
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
@@ -31,11 +37,15 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"],
+  };
   res.render("urls_index", templateVars);
 });
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
+    username: req.cookies["username"],
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
   };
